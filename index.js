c04b1287d62c5f7a33e5c60b2f4dcf3cb70277f7
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-04-10 07:37:30 +1200

    add more examples

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
-anime({
-    targets: '.item',
-    translateX: 250,
-    scaleX: 3,
-    loop: true,
-})
+// Move in a square while changing size, 1 
+// anime({
+//     targets: '.item',
+//     keyframes: [
+//         { translateX: 250, scale: 1 },
+//         { translateY: 50, scale: 1.5 },
+//         { translateX: -250, scale: 2 },
+//         { translateY: -50, scale: 2.5 },
+//     ],
+//     duration: 3000,
+//     loop: true
+// })
+
+// Staggering X translation, 5
+// anime({
+//     targets: '.item',
+//     translateX: anime.stagger(150),
+//     delay: anime.stagger(500, { from: 'last' }),
+//     duration: 3000,
+//     loop: true
+// })
+
+// Grid of items, 70
+// anime({
+//     targets: '.item',
+//     translateX: anime.stagger(10, { grid: [10, 7], from: 'center', axis: 'x' }),
+//     translateY: anime.stagger(10, { grid: [10, 7], from: 'center', axis: 'y' }),
+//     rotateZ: anime.stagger([0, 90], { grid: [10, 7], from: 'center', axis: 'x' }),
+//     delay: anime.stagger(200, { grid: [10, 7], from: 'center', }),
+//     loop: true,
+//     direction: 'alternate',
+//     easing: 'easeInOutQuad',
+// })
+
+
+// let timeline = anime.timeline({
+//     duration: 750,
+//     loop: true,
+//     diretion: 'alternate'
+// })
+// timeline.add({
+//     targets: '.item',
+//     translateX: anime.stagger(10, { grid: [10, 7], from: 'center', axis: 'x' }),
+//     translateY: anime.stagger(10, { grid: [10, 7], from: 'center', axis: 'y' }),
+//     rotateZ: anime.stagger([0, 90], { grid: [10, 7], from: 'center', axis: 'x' }),
+//     delay: anime.stagger(200, { grid: [10, 7], from: 'center', }),
+//     easing: 'easeInOutQuad',
+// }).add({
+//     targets: '.item',
+//     translateX: anime.stagger(20, { grid: [10, 7], from: 'center', axis: 'x' }),
+//     translateY: anime.stagger(20, { grid: [10, 7], from: 'center', axis: 'y' }),
+//     rotateZ: anime.stagger([90, 180], { grid: [10, 7], from: 'center', axis: 'x' }),
+//     delay: anime.stagger(200, { grid: [10, 7], from: 'center', }),
+//     easing: 'easeInOutQuad',
+// })
