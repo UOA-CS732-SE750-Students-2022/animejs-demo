3b0f69059c80970d1b253e84b01122b5c73a46dc
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-04-10 22:03:51 +1200

    add more comment messages

--- advanced/advanced.js
+++ advanced/advanced.js
@@ -1,33 +1,9 @@
-// let path = anime.path('#demo-svg path');
-// anime({
-//     targets: '#emoji',
-//     translateX: path('x'),
-//     translateY: path('y'),
-//     // rotate: path('angle'),
-//     easing: 'linear',
-//     duration: 5000,
-//     loop: true
-// })
-
-
-// anime({
-//     targets: '#demo-svg polygon',
-//     points: [
-//         { value: '87 472,87 91,267 80,501 78,489 304,500 450,293 457' },
-//         { value: '126 263,281 317,475 132,405 417,530 535,29 490,202 365' }
-//     ],
-//     easing: 'easeOutQuad',
-//     duration: 2500,
-//     loop: true,
-//     direction: 'alternate'
-// })
-
+// Animates the text (in the form of a path) to trace the outline. 
+// You can adjust the way/speed it traces by changing `stroke-dasharray` in the css file.
+// Try out different values and see what happens!
 anime({
     targets: '#demo-svg path',
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutQuad',
-    duration: 5000,
-    direction: 'alternate',
-    loop: true
-
+    duration: 8000,
 })
