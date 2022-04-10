8111c27907cb8f90bee4c333269881cc870c721a
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-04-10 18:49:48 +1200

    add some animations to logo

--- logo/logo.js
+++ logo/logo.js
@@ -1,1 +1,30 @@
+const staggerTranslateY = {
+    targets: ".char",
+    translateY: 30,
+    delay: anime.stagger(50),
+    direction: 'alternate',
+    easing: 'easeInOutSine',
+}
+
+
+const drawCharOutlines = {
+    targets: '.char path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutQuad',
+    delay: anime.stagger(150)
+}
+
+const fillChars = {
+    targets: '.char path',
+    delay: anime.stagger(50),
+    easing: 'easeInOutQuad',
+    fill: '#000',
+
+}
+
+const timeline = anime.timeline({
+    loop: true,
+})
+
+timeline.add(drawCharOutlines).add(fillChars, '-=1000').add(staggerTranslateY, '-=1500')
 
